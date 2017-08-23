325677cd16695e9e4b7c69a45dce9c1846a73838
Quentin Boussard <quentin.bouss@gmail.com>
2017-08-23 13:55:40 +0200

    init easy mode

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,13 @@ import { StatusBar } from '@ionic-native/status-bar';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+import { EasyPage } from '../pages/easy/easy';
 
 @NgModule({
   declarations: [
     MyApp,
-    HomePage
+    HomePage,
+    EasyPage
   ],
   imports: [
     BrowserModule,
@@ -19,7 +21,8 @@ import { HomePage } from '../pages/home/home';
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    HomePage
+    HomePage,
+    EasyPage
   ],
   providers: [
     StatusBar,
