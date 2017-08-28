79fec633f5c8758f2a75ae0c422a946f7f95798f
Quentin Boussard <quentin.bouss@gmail.com>
2017-08-28 11:17:35 +0200

    remove comment

--- src/pages/nav/nav.ts
+++ src/pages/nav/nav.ts
@@ -5,12 +5,7 @@ import { Slides } from 'ionic-angular';
 
 import { HomePage } from '../home/home';
 import { EasyPage } from '../easy/easy';
-/**
- * Generated class for the NavPage page.
- *
- * See http://ionicframework.com/docs/components/#navigation for more info
- * on Ionic pages and navigation.
- */
+
 
 @Component({
   selector: 'page-nav',
@@ -25,11 +20,6 @@ export class NavPage {
   goToSlide() {
     this.slides.slideTo(2, 500);
   }
-
-  // goToOtherPage(nextPage) {
-  //   console.log(nextPage);
-  //   // this.navCtrl.push(nextPage);
-  // }
 
   goToEasyPage() {
     this.navCtrl.push(EasyPage);
