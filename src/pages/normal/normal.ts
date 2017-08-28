1c62a9d92fe7309d5d4b37447f1cc78c3c0d992a
Quentin Boussard <quentin.bouss@gmail.com>
2017-08-28 17:30:47 +0200

    edit css for new page name

--- src/pages/normal/normal.ts
+++ src/pages/normal/normal.ts
@@ -2,8 +2,8 @@ import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
 @Component({
-  selector: 'page-home',
-  templateUrl: 'home.html'
+  selector: 'page-normal',
+  templateUrl: 'normal.html'
 })
 export class NormalPage {
   constructor(public navCtrl: NavController) {}
